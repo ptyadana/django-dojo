442abe55de2e25fbcf0bde19e9ab158b159c8d65
Phone Thiri Yadana <ptyadana@users.noreply.github.com>
2020-03-23 17:01:56 +0800

    project 2 update url

--- level 01/project_02/project_02/urls.py
+++ level 01/project_02/project_02/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('AppTwo.urls')),
-    path('app_two/',include('AppTwo.urls')),
+    path('help/',include('AppTwo.urls')),
 ]
